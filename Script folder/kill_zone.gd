bc9be0c25383b56fbab297cad9bde0ddcf3a765a
Adis Tabakovic <adistabakovic619@yahoo.com>
2026-02-02 17:05:11 -0800

    Made coin pick and killzones

--- Script folder/kill_zone.gd
+++ Script folder/kill_zone.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
 
+@onready var timer: Timer = $Timer
 
 
 func _on_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
