a1fd42e9c1943de0d3daae8f4858600e52c7fa7a
Adis Tabakovic <adistabakovic619@yahoo.com>
2026-02-03 19:29:38 -0800

    attempted Animation

--- Script folder/platform.gd
+++ Script folder/platform.gd
@@ -1,1 +1,8 @@
 extends AnimatableBody2D
+
+@onready var plat_animation: AnimationPlayer = $PlatAnimation
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	plat_animation.play("new_animation")
